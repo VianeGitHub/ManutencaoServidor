d06e4dfc95555d3cfa2f3283cfcc9a6ad69820e7
Viane Silva <vianeprofissional@gmail.com>
2025-09-22 19:51:51 -0300

    feat: Subindo projeto

---
+++ backend/src/main/java/com/manutencaoservidor/manutencaoservidor/ManutencaoservidorApplication.java
@@ -0,0 +1,13 @@
+package com.manutencaoservidor.manutencaoservidor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManutencaoservidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ManutencaoservidorApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/manutencaoservidor/manutencaoservidor/ManutencaoservidorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manutencaoservidor.manutencaoservidor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ManutencaoservidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
